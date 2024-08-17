9fcba05b753c54728b5cd1c5f8c1919b6e894065
Ahmad Hanafi <83533499+ahmad123365@users.noreply.github.com>
2024-08-17 21:05:20 +0200

    initial commit

---
+++ App.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import {
+  SafeAreaView,
+  ScrollView,
+  StatusBar,
+  StyleSheet,
+  Text,
+} from 'react-native';
+import Navigator from './src/routes/homeStack';
+
+function App() {
+  return <>{/* <Navigator /> */}</>;
+}
+
+const styles = StyleSheet.create({});
+
+export default App;
--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,19 @@
-module.exports = {
-  presets: ['module:@react-native/babel-preset'],
+module.exports = function () {
+  return {
+    presets: ['module:@react-native/babel-preset'],
+    plugins: [
+      'react-native-reanimated/plugin',
+      [
+        'module-resolver',
+        {
+          root: ['./'],
+          extensions: ['.ios.js', '.android.js', '.js', '.ts', '.tsx', '.json'],
+          alias: {
+            '@src': './src',
+            '@res': './res',
+          },
+        },
+      ],
+    ],
+  };
 };
